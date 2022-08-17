b83975327dad8ffe8c7d323bcc5f912291353905
Dinesh Peshwani <dinesh.peshwani.20.1@mountblue.tech>
2022-08-17 17:16:41 +0530

    navbar element spacing refactored

--- todoist/src/Components/Navbar.jsx
+++ todoist/src/Components/Navbar.jsx
@@ -1,22 +1,49 @@
 import React, { Component } from "react";
-import { Flex, Input } from "@chakra-ui/react";
-import "../App.css";
+import {
+  Center,
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+// import "../App.css";
 import Sidebar from "./Sidebar";
 
 class Navbar extends Component {
   render() {
     return (
-      <Flex>
-        <Flex bg="#DB4C3F" w="100vw" h="10vh" justifyContent= "space-around" justifyItems="center" alignItems="center">
-          <Sidebar/>
+      <Flex 
+          bg="#DB4C3F"
+          w="100vw"
+          h="10vh"
+          alignItems="center">
+        <Flex w="150%" justifyContent="space-around" >
+          <Sidebar />
+          <Heading color="white">Todoist</Heading>
+        </Flex>
+          <InputGroup>
+            <InputLeftElement
+            ml="7.5rem" 
+              children={
+                <img src="https://res.cloudinary.com/dct4net75/image/upload/v1660734090/search-9-24_uv4x81.png" />
+              }
+            />
             <Input
+              w="45%"
+            ml="7.5rem" 
+
+              className="searchbar-icon"
               type="search"
               bg="#FFFFFF33"
-              width= "40%"
-              placeholder="Search Projects"
+              width="10%"
+              variant="flushed"
+              border="none"
+              borderRadius="2rem"
+              placeholder={``}
               textColor={"white"}
             />
-        </Flex>
+          </InputGroup>
       </Flex>
     );
   }
